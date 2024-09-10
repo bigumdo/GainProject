8b6cff8e994f74760be2d46c4989bb1a1897fd42
bigumdo <showmethe0610@gmail.com>
2024-09-10 23:39:54 +0900

    PlayerStaetMachine

--- Assets/01.Script/Agent/Player/Player.cs
+++ Assets/01.Script/Agent/Player/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,7 +10,35 @@ public class Player : Agent
 
     [Header("Setting")]
     private float _moveSpeed;
+    
+    public PlayerStateMachine StateMachine { get; protected set; }
 
+    protected void Awake()
+    {
+        base.Awake();
+        StateMachine = new PlayerStateMachine();
+        
+        foreach(PlayerStateEnum stateEnum in Enum.GetValues(typeof(PlayerStateEnum)))
+        {
+            string typeName = stateEnum.ToString();
 
+            try
+            {
+                Type t = Type.GetType($"Player{typeName}State");
+                PlayerState state = Activator.CreateInstance(
+                    t, this, StateMachine, typeName) as PlayerState;
+                StateMachine.AddState(stateEnum, state);
+            }catch(Exception ex)
+            {
+                Debug.LogError($"{typeName} is loading error! check Message");
+                Debug.LogError(ex.Message);
+            }
+        }
 
+    }
+
+    public void Update()
+    {
+        
+    }
 }
--- Assets/01.Script/Agent/Player/PlayerStateMachine.cs
+++ Assets/01.Script/Agent/Player/PlayerStateMachine.cs
@@ -2,21 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class PlayerStateMachine 
+public class PlayerStateMachine 
 {
     
-    Dictionary<PlayerStateEnum,PlayerState> stateDictionary;
-    
-    
+    private Dictionary<PlayerStateEnum,PlayerState> stateDictionary;
+    public PlayerState CurrentState{ get; private set; }
+    private Player _player;
+
+    public PlayerStateMachine()
+    {
+        stateDictionary = new Dictionary<PlayerStateEnum,PlayerState>();
+    }
+
+    public void Initialize(PlayerStateEnum startState,Player player )
+    {
+        _player = player;
+        CurrentState = stateDictionary[startState];
+        CurrentState.Enter();
+    }
 
     public void ChangeState(PlayerStateEnum state)
     {
-        
+        CurrentState.Exit();
+        CurrentState = stateDictionary[state];
+        CurrentState.Enter();
     }
     
-    public void AddState()
+    public void AddState(PlayerStateEnum stateEnum,PlayerState state)
     {
-
+        stateDictionary.Add(stateEnum,state);
     }
 
 }
