4cd98593e1c42b162f985db77fb37892da589625
bigumdo <showmethe0610@gmail.com>
2024-11-10 23:50:14 +0900

    WeaponRotation

--- Assets/01.Script/Agent/Player/PlayerWeapon/CurrentWeapon.cs
+++ Assets/01.Script/Agent/Player/PlayerWeapon/CurrentWeapon.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class CurrentWeapon : MonoBehaviour
 {
-    
+    private Weapon _weapon;
+
+    private void Awake()
+    {
+        _weapon = GetComponentInChildren<Weapon>();
+    }
+
+    private void Update()
+    {
+        Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        Debug.Log(mouse);
+        Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        float angle = Mathf.Atan2(mouse.y, mouse.x) * Mathf.Rad2Deg;
+        transform.localRotation = Quaternion.AngleAxis(angle, transform.root.right);
+
+    }
+
+
 }
